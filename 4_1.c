baf3a42d69e7361c6916a4913b6d6e0cda49e9b5
snjain22 <118072637+snjain22@users.noreply.github.com>
2023-08-31 16:08:58 +0530

    Update 4_1.c
    Finalized Code - fully working

--- 4_1.c
+++ 4_1.c
@@ -1,75 +1,142 @@
-/*1)	Implement a menu driven program to define a stack of characters. 
-Include push, pop and display functions. 
-Also include functions for checking error conditions such as underflow and overflow 
-(ref. figure 1) by defining isEmpty and isFull functions. 
-Use these function in push, pop and display functions appropriately. 
-Use type defined structure to define a STACK containing a 
+/*1)	Implement a menu driven program to define a stack of characters.
+Include push, pop and display functions.
+
+Also include functions for checking error conditions such as underflow and overflow
+(ref. figure 1) by defining isEmpty and isFull functions.
+
+Use these function in push, pop and display functions appropriately.
+
+Use type defined structure to define a STACK containing a
 character array and an integer top. Do not use global variables.*/
-#include<stdio.h>
+
+#include <stdio.h>
+#include <stdlib.h>
 
 #define MAX 10
 
-
-typedef struct 
+typedef struct
 {
     int TOP;
     char Arr[MAX];
 } STACK;
 
+void isEmpty()
+{
+    printf("STACK IS EMPTY!\n");
+}
 
+void isFull()
+{
+    printf("STACK IS FULL!\n");
+}
 
 void Push(STACK *p, char X)
 {
-    if (p->TOP == MAX-1)
+    if (p->TOP == MAX - 1)
+    {
+        isFull();
         printf("OverFlow! ");
+    }
     else
+    {
+        p->TOP += 1;
         (p->Arr[p->TOP]) = X;
-    
+    }
 }
+
+
 
 int POP(STACK *p)
 {
-    if ((p->TOP)==0)
+    if ((p->TOP) == -1)
     {
+        isEmpty();
         printf("UNDERFLOW!");
     }
     else
     {
-        p->TOP = (p->TOP)--;
-        return p->TOP;
+        printf("Popped Element: %c\n", p->Arr[p->TOP]);
+        p->TOP--;
+        
     }
 }
 
 int Display(STACK *p)
 {
-    for(int i=p->TOP ; i>=0 ; i-- )
+    if (p->TOP == -1)
     {
-        printf("| %d |\n------", p->Arr[i]);
+        isEmpty();
+    }
+
+    else
+    {        
+        for (int i = p->TOP; i >= 0; i--)
+        {
+            printf("| %c |\n", p->Arr[i]);
+        }
     }
 }
 
 void main()
 {
-    STACK S, *p;
-    int n;
+    STACK S;
+    int n, ch;
     char X;
-    printf("Enter No.: ");
-    scanf("%d", &n);
-    S.TOP=-1;
+    S.TOP = -1;
 
-    // for (int i = 0 ; i<n ; i++)
-    // {
-        scanf("%d", &X);
-        // Push(&S,X);
-    // }
-    if (p->TOP == MAX-1)
-        printf("OverFlow! ");
-    else
+    while (1)
     {
-        printf("TOP: %d\n", S.TOP);
-        printf("%d", p->Arr[p->TOP++]);
-        
+        printf("--MENU--\nEnter\n1 - Pushing Elements\n2-Popping Elements\n3-Displaying\n4-Exit\n");
+        printf("Enter Choice: ");
+        scanf("%d", &ch);
+
+        if (ch == 1)
+        {
+            printf("--Pushing Elements!--\n");
+            printf("Enter Num of Elements: ");
+            scanf("%d", &n);
+            for (int i = 0; i < n; i++)
+            {
+                printf("Enter Element %d: ", i + 1);
+                scanf(" %c", &X);
+                Push(&S, X);
+            }
+        }
+
+        else if (ch == 2)
+        {
+            printf("--Popping Elements--\n");
+            printf("Enter Number of Elements: ");
+            scanf("%d", &n);
+            if (S.TOP+1 - n >= 0)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    POP(&S);
+                }
+            }
+
+            else if( S.TOP==-1)
+            {
+                POP(&S);
+            }
+            else
+            {
+                printf("You cannot delete %d elements from a stack of length %d!", n, S.TOP+1);
+            }
+        }
+
+        else if (ch == 3)
+        {
+            printf("--Displaying Elements--\n");
+            Display(&S);
+        }
+
+        else if (ch == 4)
+        {
+            printf("--Exiting--");
+            break;
+        }
     }
 
-    Display(&S);
 }
