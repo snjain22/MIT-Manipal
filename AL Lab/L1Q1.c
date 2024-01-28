0644b19250618cdc50bba5733748dcdc3b691655
snjain22 <118072637+snjain22@users.noreply.github.com>
2024-01-29 00:42:01 +0530

    Update L1Q1.c

--- AL Lab/L1Q1.c
+++ AL Lab/L1Q1.c
@@ -1,1 +1,37 @@
+#include<stdio.h>
+#include<stdlib.h>
 
+void main()
+{
+    int n;
+    printf("Enter Number of Elements: ");
+    scanf("%d", &n);
+
+    int arr[n], temp, OC=0;
+
+    printf("Enter Elements: ");
+    
+    for(int i = 0 ; i<n ; i++){
+        printf("Enter %dth Element: ", i);
+        scanf("%d", &arr[i]);
+    }
+
+    for (int i = 0 ; i<n ; i++){
+        for(int j = 0 ; j<n-i-1 ; j++){
+            
+            OC++;
+
+            if (arr[j] > arr[j+1]){           
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+
+    printf("ARRAY AFTER SORTING: \n");
+    for(int i = 0 ; i < n ; i++){
+        printf("%d ", arr[i]);
+    }
+    printf("OpCount: %d", OC);
+}
